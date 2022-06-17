c6573c083f40df61c007fac739e7de1ee6480d85
Francois van Leersum <13562583+Blockshot12@users.noreply.github.com>
2022-06-17 07:42:43 +0200

    Update logo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,59 +1,59 @@
-import React, { useState } from 'react'
-import Head from 'next/head'
-import { SocialIcon } from './SocialIcons'
+import React, { useState } from "react";
+import Head from "next/head";
+import { SocialIcon } from "./SocialIcons";
 import {
   LineItemsContainer,
   LineItemsCount,
-} from '@commercelayer/react-components'
-import ShoppingBag from './ShoppingBag'
-import LayoutContext from '@context/LayoutContext'
-import LanguageSelector from './LanguageSelector'
-import CountrySelector from './CountrySelector'
-import { Country } from '@typings/models'
-import { Transition } from '@headlessui/react'
-import Link from 'next/link'
-import locale from '@locale/index'
+} from "@commercelayer/react-components";
+import ShoppingBag from "./ShoppingBag";
+import LayoutContext from "@context/LayoutContext";
+import LanguageSelector from "./LanguageSelector";
+import CountrySelector from "./CountrySelector";
+import { Country } from "@typings/models";
+import { Transition } from "@headlessui/react";
+import Link from "next/link";
+import locale from "@locale/index";
 
-const getCmsHref = (cms: 'contentful' | 'dato' | string) => {
+const getCmsHref = (cms: "contentful" | "dato" | string) => {
   switch (cms) {
-    case 'contentful':
-      return '//contentful.com'
-    case 'dato':
-      return '//datocms.com'
+    case "contentful":
+      return "//contentful.com";
+    case "dato":
+      return "//datocms.com";
     default:
-      return '#'
+      return "#";
   }
-}
+};
 
 type Props = {
-  title?: string
-  socialIcons?: SocialIcon[]
-  showMenu?: boolean
-  lang?: string
-  buildLanguages?: Country[]
-  countries?: Country[]
-  cms: string
-}
+  title?: string;
+  socialIcons?: SocialIcon[];
+  showMenu?: boolean;
+  lang?: string;
+  buildLanguages?: Country[];
+  countries?: Country[];
+  cms: string;
+};
 
 const Layout: React.FunctionComponent<Props> = ({
   children,
-  title = 'This is the default title',
+  title = "This is the default title",
   showMenu = true,
-  lang = 'en-us',
+  lang = "en-us",
   buildLanguages = [],
   countries = [],
   cms,
 }) => {
-  const [animation, setAnimation] = useState(false)
-  const [burgerMenu, setBurgerMenu] = useState(false)
+  const [animation, setAnimation] = useState(false);
+  const [burgerMenu, setBurgerMenu] = useState(false);
   const handleAnimation = (event: React.MouseEvent) => {
-    event.preventDefault()
-    setAnimation(!animation)
-  }
-  const cmsHref = getCmsHref(cms)
+    event.preventDefault();
+    setAnimation(!animation);
+  };
+  const cmsHref = getCmsHref(cms);
   const opacity = animation
-    ? 'opacity-25 transition ease-in duration-300'
-    : 'transition ease-in duration-300'
+    ? "opacity-25 transition ease-in duration-300"
+    : "transition ease-in duration-300";
   return (
     <LayoutContext.Provider value={{ handleAnimation }}>
       <Head>
@@ -68,7 +68,11 @@ const Layout: React.FunctionComponent<Props> = ({
           href="//data.commercelayer.app/assets/fonts/fontawesome.min.css"
           rel="stylesheet"
         />
-        <link rel="shortcut icon" type="image/x-icon" href="https://data.commercelayer.app/assets/images/favicons/favicon.ico"/>
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href="https://data.commercelayer.app/assets/images/favicons/favicon.ico"
+        />
         {/* <link
           href="//data.commercelayer.app/assets/css/commercelayer.min.css"
           rel="stylesheet"
@@ -94,6 +98,7 @@ const Layout: React.FunctionComponent<Props> = ({
                       />
                     </a>
                   </Link>
+                  <p className="ml-2">Demo</p>
                   {showMenu && (
                     <div className="-mr-2 flex items-center md:hidden">
                       <button
@@ -222,7 +227,7 @@ const Layout: React.FunctionComponent<Props> = ({
         <main>{children}</main>
         <footer className={`mt-12 border-t border-gray-200 py-8 ${opacity}`}>
           <p className="text-xs mx-5 sm:mx-0 sm:text-base text-gray-400 text-center">
-            Powered by{' '}
+            Powered by{" "}
             <a
               className="underline hover:text-blue-500"
               href="//commercelayer.io"
@@ -230,7 +235,7 @@ const Layout: React.FunctionComponent<Props> = ({
             >
               Commerce Layer
             </a>
-            ,{' '}
+            ,{" "}
             <a
               href={cmsHref}
               target="_blank"
@@ -238,15 +243,15 @@ const Layout: React.FunctionComponent<Props> = ({
             >
               {cms}
             </a>
-            , and{' '}
+            , and{" "}
             <a
               className="underline hover:text-blue-500"
               href="//nextjs.org"
               target="_blank"
             >
               Next.js
-            </a>{' '}
-            on{' '}
+            </a>{" "}
+            on{" "}
             <a
               className="underline hover:text-blue-500"
               href="//netlify.com"
@@ -258,7 +263,7 @@ const Layout: React.FunctionComponent<Props> = ({
         </footer>
       </div>
     </LayoutContext.Provider>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
